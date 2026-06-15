6acf9726b4b3dbee001997e6bfbf80e8a715e9b6
Nayef-Abou-Tayoun <nayef.abou.tayoun@ibm.com>
2026-06-14 20:55:02 -0400

    Delete plugins/token_cost_calculator directory

--- plugins/token_cost_calculator/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Token Cost Calculator Plugin."""
-
-from plugins.token_cost_calculator.token_cost_calculator import TokenCostCalculatorPlugin
-
-__all__ = ["TokenCostCalculatorPlugin"]
-
-# Made with Bob
